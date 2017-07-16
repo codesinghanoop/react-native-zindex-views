4fecd0a0360bed79b26d750218865239cd0dacd0
Anoop Singh <>
2017-07-17 01:12:29 +0530

    Code clean

--- Example/zIndexedView.js
+++ Example/zIndexedView.js
@@ -6,21 +6,11 @@ import {
     Animated,
     Dimensions
 } from 'react-native'
-import ArrowComponent from './util.js'
+import ArrowComponent from 'react-native-zindex-views'
 var fullViewHeight = Dimensions.get('window').height;
 var fullViewWidth = Dimensions.get('window').width;
 var _this
 let placement = 'bottom'
-
-function Point(x, y) {
-  this.x = x;
-  this.y = y;
-}
-
-function Size(width, height) {
-  this.width = width;
-  this.height = height;
-}
 
 export default class ZindexedView extends Component {
     constructor(props)
@@ -62,61 +52,9 @@ export default class ZindexedView extends Component {
     })   
 }
 
-//   getArrowSize(placement) {
-//     var size = new Size(10, 5);
-//     switch(placement) {
-//       case 'left':
-//       case 'right':
-//         return new Size(size.height, size.width);
-//       default:
-//         return size;
-//     }
-//   }
-
-// computeGeometry({contentSize, placement}) {
-//     placement = 'bottom'
-//     const {x,y,px,py,mwidth,mheight} = _this.state
-//     console.log('the coordinates are',x,y,px,py,mwidth,mheight)
-//     var options = {
-//       displayArea: {x: 5, y: 20, width: width - 10, height: height - 25},
-//       fromRect: {x: px, y: py, width: mwidth, height: mheight},
-//       arrowSize: _this.getArrowSize(placement),
-//       contentSize,
-//     }
-//     console.log('the placement is',options)
-//     switch (placement) {
-//       case 'top':
-//         return _this.computeTopGeometry(options);
-//       case 'bottom':
-//         return _this.computeBottomGeometry(options);
-//       case 'left':
-//         return _this.computeLeftGeometry(options);
-//       case 'right':
-//         return _this.computeRightGeometry(options);
-//       default:
-//         return _this.computeAutoGeometry(options);
-//     }
-//   }
-
-//    computeBottomGeometry({displayArea, fromRect, contentSize, arrowSize}) {
-//     var popoverOrigin = new Point(
-//       Math.min(displayArea.x + displayArea.width - contentSize.width,
-//         Math.max(displayArea.x, fromRect.x + (fromRect.width - contentSize.width) / 2)),
-//       fromRect.y + fromRect.height + arrowSize.height);
-//     var anchorPoint = new Point(fromRect.x + fromRect.width / 2.0, fromRect.y + fromRect.height);
-
-//     return {
-//       popoverOrigin,
-//       anchorPoint,
-//       placement: 'bottom',
-//     }
-//   }
 
   render()
   {
-    //   console.log('the GEOM',this.state.geom)
-    //   const {x,y,px,py,mwidth,mheight,popoverOrigin,anchorPoint} = this.state
-       console.log('the call is for menu',this.state.contentSize,this.state.belowWhichViewDimensions)
        var displayArea = {x: 5, y: 20, width: fullViewWidth - 10, height: fullViewHeight - 25};
       return (
             <View style= {{flex:1,backgroundColor: 'green'}}>
